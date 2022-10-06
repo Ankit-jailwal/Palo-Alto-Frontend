0741a61f2c5c6e973219b198098c5401444fe969
Ankit-jailwal <jailwalankit@gmail.com>
2022-10-06 13:51:46 +0530

    Updates

---
+++ precache-manifest.e18eda3d6e406fc48a00a6054dcf396c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877720e221afff3f6fcea179666e8e8f",
+    "url": "/Palo-Alto-Frontend/index.html"
+  },
+  {
+    "revision": "7d814052db82be895864",
+    "url": "/Palo-Alto-Frontend/static/css/main.516896ae.chunk.css"
+  },
+  {
+    "revision": "196c66e6e0d5b0063af4",
+    "url": "/Palo-Alto-Frontend/static/js/2.524419cd.chunk.js"
+  },
+  {
+    "revision": "cbf4a068c2830df381d397845cff4edb",
+    "url": "/Palo-Alto-Frontend/static/js/2.524419cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d814052db82be895864",
+    "url": "/Palo-Alto-Frontend/static/js/main.729d96a1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Palo-Alto-Frontend/static/js/main.729d96a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907ed0996a50f7f9b9da",
+    "url": "/Palo-Alto-Frontend/static/js/runtime-main.d5fa98d1.js"
+  },
+  {
+    "revision": "039a438109fe0689eb6f37d330259464",
+    "url": "/Palo-Alto-Frontend/static/media/close.039a4381.svg"
+  },
+  {
+    "revision": "9990a52ecbe0459db94a063a1bd1fd37",
+    "url": "/Palo-Alto-Frontend/static/media/menu.9990a52e.svg"
+  }
+]);
